498223d5f816880fe5e0aaf6e128ea753b44244a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-21 22:33:25 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/gsandroidkotlin/ItemDao.kt
@@ -0,0 +1,20 @@
+package com.example.gsandroidkotlin
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ItemDao {
+
+    @Query("SELECT * FROM ItemModel")
+    fun getAll(): LiveData<List<ItemModel>>
+
+    @Insert
+    fun insert(item: ItemModel)
+
+    @Delete
+    fun delete(item: ItemModel)
+}
---
+++ app/src/main/java/com/example/gsandroidkotlin/ItemModel.kt
@@ -0,0 +1,12 @@
+package com.example.gsandroidkotlin
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ItemModel(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String
+)
